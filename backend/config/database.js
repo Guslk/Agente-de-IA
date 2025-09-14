fa57a173d0f217ec16273608e56239d94e00b9ca
Math0106 <140568929+Math0106@users.noreply.github.com>
2025-09-14 15:51:51 -0300

    Teste matheus

---
+++ backend/config/database.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+require('dotenv').config(); // Carrega as variáveis do arquivo .env
+
+// const MONGO_URI = process.env.MASTER_MONGO_URI;
+const MONGO_URI = process.env.MASTER_MONGO_URI;
+
+// Função assíncrona para conectar ao banco de dados
+const connectMasterDB = async () => {
+  try {
+    // Validação para garantir que a URI foi carregada
+    if (!MONGO_URI) {
+      throw new Error("A URI do MongoDB Master não foi definida no arquivo .env");
+    }
+
+    // Tenta conectar ao MongoDB usando Mongoose
+    await mongoose.connect(MONGO_URI);
+
+    console.log('✅ Conexão com o Banco de Dados Master (MongoDB) estabelecida com sucesso!');
+
+  } catch (error) {
+    // Se ocorrer um erro, exibe o erro e encerra a aplicação
+    console.error('❌ Erro ao conectar ao Banco de Dados Master:', error.message);
+    
+    // É crucial encerrar o processo se não for possível conectar ao banco de dados principal.
+    // A aplicação não pode funcionar sem ele.
+    process.exit(1); 
+  }
+};
+
+module.exports = connectMasterDB;
